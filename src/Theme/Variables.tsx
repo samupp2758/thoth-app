f4856ceea864a69dacb676abd51500f2dab2b905
Samuel Macedo Vargas <41263908+samupp2758@users.noreply.github.com>
2022-12-25 20:33:08 -0300

    Added Colors to Theme/Variables

---
+++ src/Theme/Variables.tsx
@@ -0,0 +1,29 @@
+
+
+
+export const Colors = {
+    // Example colors:
+    transparent: 'rgba(0,0,0,0)',
+    white: '#ffffff',
+    text: '#525257',
+    textDarker:'#4006A9',
+    textLight:'#FAFAFA',
+    inputBackground:'#F0F0F0',
+    inputBorder:'#7638E8',
+    placeHolder:'#C4C4C4',
+    borderNotFound:'#8A8A8A',
+    primaryHighLighted:'#E6D9FF',
+    primary: '#E14032',
+    success: '#1B9C37',
+    error: '#DF3C3C',
+    buttonTitle:'#29292E',
+  }
+  
+  /**
+   * FontSize
+   */
+  export const FontSize = {
+    small: 16,
+    regular: 20,
+    large: 40,
+  }
