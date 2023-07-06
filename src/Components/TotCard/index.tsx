e65357861cef2a09ee56c7f8cefcb89cd52a82d5
Samuel Macedo Vargas <41263908+samupp2758@users.noreply.github.com>
2023-07-05 23:10:18 -0300

    Added touchable container to totcard

--- src/Components/TotCard/index.tsx
+++ src/Components/TotCard/index.tsx
@@ -3,41 +3,58 @@ import { useTheme } from '../../Hooks'
 import { ButtonedIcon } from '../Button'
 import Icon from '../Icon'
 import { Text } from '../Text'
-import { Container,
-    Box,
-    ContentContainer } from "./style"
+import { Container, Box, ContentContainer } from './style'
+import { TouchableOpacity } from 'react-native'
 
 type Props = {
-    title?:string,
-    background?:string,
-    saved?:boolean,
-    topic?:string,
-    key?:string
+  title?: string
+  background?: string
+  saved?: boolean
+  topic?: string
+  key?: string
 }
 
 export default ({
-    title = "",
-    background = "",
-    saved = false,
-    topic = true
+  title = '',
+  background = '',
+  saved = false,
+  topic = true
 }) => {
-    const [bookmark,setbookmark] = useState(saved? 'bookmark': 'bookmark-o')
+  const [bookmark, setbookmark] = useState(saved ? 'bookmark' : 'bookmark-o')
 
-    const save = () => {
-        setbookmark(bookmark =='bookmark' ? 'bookmark-o': 'bookmark')
-    }
+  const save = () => {
+    setbookmark(bookmark == 'bookmark' ? 'bookmark-o' : 'bookmark')
+  }
 
-    return(<Container>
-                <Box>
-                    {<ButtonedIcon onPress={()=>save()} color={useTheme().Colors.textLight} name={bookmark} family='FontAwesome'/>}
-                </Box>
-                <ContentContainer>
-                    <Text bold 
-                    fontSize={useTheme().FontSize.regularHalf}
-                    color={useTheme().Colors.textLight}>{title}</Text>
-                    <Text
-                    fontSize={useTheme().FontSize.regular}
-                    color={useTheme().Colors.textLight}>{topic}</Text>
-                </ContentContainer>
-            </Container>)
+  return (
+    <TouchableOpacity onPress={() => {}}>
+      <Container>
+        <Box>
+          {
+            <ButtonedIcon
+              onPress={() => save()}
+              color={useTheme().Colors.textLight}
+              name={bookmark}
+              family='FontAwesome'
+            />
+          }
+        </Box>
+        <ContentContainer>
+          <Text
+            bold
+            fontSize={useTheme().FontSize.regularHalf}
+            color={useTheme().Colors.textLight}
+          >
+            {title}
+          </Text>
+          <Text
+            fontSize={useTheme().FontSize.regular}
+            color={useTheme().Colors.textLight}
+          >
+            {topic}
+          </Text>
+        </ContentContainer>
+      </Container>
+    </TouchableOpacity>
+  )
 }
